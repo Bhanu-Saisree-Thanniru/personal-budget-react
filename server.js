4c7e95dc7b9ec215400e5825dba90eb3d14cbe12
Bhanu Saisree Thanniru <thanniru.bhanusaisree@gmail.com>
2024-02-21 20:46:53 -0500

    Modified changes

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+const config = require('./jsonData.json');
+
+
+app.use(cors());
+
+const budget = {
+    myBudget: [
+        {
+            title: 'Eat out',
+            budget: 25
+        },
+        {
+            title: 'Rent',
+            budget: 275
+        },
+        {
+            title: 'Grocery',
+            budget: 110
+        },
+    ]
+};
+
+
+app.get('/budget', (req, res) => {
+    res.send(config);
+});
+
+app.listen(port, () => {
+    console.log(`API served at http://localhost:${port}`);
+});
